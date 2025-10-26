2106a2b82b2f33bd30e40c5f6117af3903cc5abc
Mostafa Mohammed <engmostafaaboelalla@gmail.com>
2025-10-26 15:46:26 +0300

    first commit added new git local repositry

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my_first_angular_project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('mostada mohammed ahmmed');
   });
 });
